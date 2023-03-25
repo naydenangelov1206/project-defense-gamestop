1293f4e6147c257c3650d91dd9e5ca14da2a0827
naydenangelov1206 <naydenangelov1206@gmail.com>
2023-03-25 18:08:02 +0200

    adding logout function to the authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,3 +4,19 @@ import * as request from "./requester";
 export function login(email, password) {
   return request.post(baseUrl + "/users/login", { email, password });
 }
+
+export async function logout(accessToken) {
+  try {
+    const res = await fetch(baseUrl + "/users/logout", {
+      headers: {
+        "X-Authorization": accessToken,
+      },
+    });
+
+    if (res.status === 204) {
+      return res;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
